1d5c87309b6875b926bdb4d55122bab64b3f25da
Ben L <39037350+blawson69@users.noreply.github.com>
2020-09-29 13:59:37 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # TimeBomb
- Creates "time bombs" in Roll20 games
+
+This [Roll20](http://roll20.net/) script allows for the creation of player-controllable "time bombs" on the Combat Tracker in Roll20 games. Each Bomb you add to the Tracker will count down each round to zero, detonating the Bomb and sending an effect dialog to all players and replacing the Bomb token with a graphic representation the detonation.
+
+## Set Up
+
+TimeBomb requires two tokens: A "Bomb" token and a "Result" token. The Bomb token provides the number of rounds to start the countdown and the detonation chat effects. The Result token is a graphic representation of the detonation effect. Once you have set up your Bomb and Result tokens (see below), select both of them and use the `!bomb create` command.
+
+### Bomb Token
+Place a graphic on the VTT to represent your Bomb. Enter "Bomb" in the first Bar 1 field to designate it as the Bomb token. This token can be controlled by any number of players if desired, and the token name can be revealed to players as you see fit.
+
+The Bomb's timer is round based. If you wish it to detonate after six rounds, enter "6" in the token's first Bar 2 field. This number will decrement by one each round. On reaching zero, the Bomb will detonate. If no number is provided, it will default to 10.
+
+The detonation chat effects are stored in the Bomb token's GM Notes field. You can give as much information as you wish here, depending on your Bomb and what should happen once it's detonated. An alternative option is to use a [roll template](https://roll20.zendesk.com/hc/en-us/articles/360037257334-How-to-Make-Roll-Templates). This allows you to provide a melee or magical attack dialog with rolled damage, saving throw links/buttons and any other information you like that matches a familiar format.
+
+If not using a roll template, you can still provide die roll expressions that will be executed whenever the Bomb is detonated. In your effect description, surround your die expression in @ signs, i.e. `@1d8+2@`. You may use as many die expressions as you want and each will be evaluated separately. You **should not** use @ signs for any other purpose or it will give unintended results.
+
+### Result Token
+Place a token on the VTT to represent the graphic effects of your Bomb. This could be a fireball, fallen debris, etc. Size and place this token as needed relative to the Bomb token. This relative location is stored for later detonation. If your Result token is centered on the Bomb token, for instance, the Result token will still be centered when the Bomb is detonated no matter where the Bomb was moved.
+
+By default, the Result token will be placed below player tokens on the map layer. This allows the players to see the Bomb's effect and still access their tokens. If you wish to place the Result token over all player tokens, enter "tokens" into the second Bar 1 field.
+
+## Starting the Countdown
+
+The Bomb you created will sit quietly until you add it to the Turn Tracker. If the Tracker is not open, TimeBomb will open it to add the Bomb. To add a Bomb to the Tracker, select the Bomb token and use the `!bomb add` command.
+
+You can add as many Bombs to the Turn Tracker as you wish, and each will count down as their turn comes up.
+
+## Detonation
+
+When a Bomb's counter reaches zero in the Turn Tracker, it will detonate. This replaces the Bomb token with the Result token, sends the chat effect to all players, and removes the Bomb from the Turn Tracker. The GM will also receive a dialog with buttons to hide the Result token (sending it to the Dynamic Lighting layer) if desired, or to reset the Bomb. Resetting a Bomb allows it to be added to the Turn Tracker again.
+
+## Detection and Disarmament
+
+Because of the many possible Bomb variants, detection and disarmament procedures are strictly left up to the GM. If you allow a Bomb to be disarmed while counting down in the Turn Tracker, you can simply delete it from the Tracker. If you want to let a failed disarm attempt to subtract rounds from the timer, the Bomb's Tracker entry can be edited like any other item.
---
+++ TimeBomb.js
@@ -0,0 +1,303 @@
+﻿/*
+TimeBomb
+Add a "time bomb" to the Combat Tracker in Roll20
+
+On Github:	https://github.com/blawson69
+Contact me: https://app.roll20.net/users/1781274/ben-l
+
+TODO:
+    make result token follow bomb token
+
+Like this script? Become a patron:
+    https://www.patreon.com/benscripts
+*/
+
+var TimeBomb = TimeBomb || (function () {
+    'use strict';
+
+    //---- INFO ----//
+
+    var version = '0.1',
+    debugMode = false,
+    styles = {
+        box:  'background-color: #fff; border: 1px solid #000; padding: 8px 10px; border-radius: 6px; margin-left: -40px; margin-right: 0px;',
+        title: 'padding: 0 0 10px 0; color: ##591209; font-size: 1.5em; font-weight: bold; font-variant: small-caps; font-family: "Times New Roman",Times,serif;',
+        button: 'background-color: #000; border-width: 0px; border-radius: 5px; padding: 5px 8px; color: #fff; text-align: center;',
+        textButton: 'background-color: transparent; border: none; padding: 0; color: #591209; text-decoration: underline;',
+        buttonWrapper: 'text-align: center; margin: 10px 0; clear: both;',
+        code: 'font-family: "Courier New", Courier, monospace; background-color: #ddd; color: #000; padding: 2px 4px;',
+        alert: 'color: #C91010; font-size: 1.5em; font-weight: bold; font-variant: small-caps; text-align: center;'
+    },
+
+    checkInstall = function () {
+        if (!_.has(state, 'TimeBomb')) state['TimeBomb'] = state['TimeBomb'] || {};
+        if (typeof state['TimeBomb'].bombs == 'undefined') state['TimeBomb'].bombs = [];
+        log('--> TimeBomb v' + version + ' <-- Primed and ready. Let\'s blow sh*t up!');
+		if (debugMode) {
+			var d = new Date();
+			showDialog('Debug Mode', 'TimeBomb v' + version + ' loaded at ' + d.toLocaleTimeString() + '.', 'GM');
+		}
+    },
+
+    //----- INPUT HANDLER -----//
+
+    handleInput = function (msg) {
+        if (msg.type == 'api' && msg.content.startsWith('!bomb') && playerIsGM(msg.playerid)) {
+			var parms = msg.content.split(/\s+/i);
+			if (parms[1]) {
+				switch (parms[1]) {
+                    case 'create':
+                        commandCreate(msg);
+                        break;
+                    case 'add':
+                        commandAdd(msg);
+                        break;
+                    case 'reset':
+                        commandReset(msg);
+                        break;
+                    case 'hide':
+                        commandHideResult(msg);
+                        break;
+                    case 'destroy':
+                        commandDestroy(msg);
+                        break;
+                    case 'help':
+                    default:
+                        commandHelp(msg);
+				}
+			} else {
+                commandHelp(msg);
+			}
+		}
+    },
+
+    commandCreate = function (msg) {
+        if (_.size(msg.selected) !== 2) {
+            showDialog('Error', 'You have not selected the right number of tokens! Please select a "Bomb" token and a "Result" token.', msg.who);
+            return;
+        }
+        var bomb, result;
+        _.each(msg.selected, function (obj) {
+            var token = getObj(obj._type, obj._id);
+            if (token) {
+                var nameVal = token.get("bar1_value") ;
+                if (nameVal === 'Bomb') bomb = token;
+                if (nameVal === 'Result') result = token;
+            }
+        });
+
+        if (typeof bomb != 'undefined' &&_.find(state['TimeBomb'].bombs, function (x) { return x == bomb.get('id'); })) {
+            showDialog('Error', '"' + bomb.get('name') + '" has already been created.', msg.who);
+            return;
+        }
+
+        if (typeof bomb != 'undefined' && typeof result != 'undefined') {
+            var lOffset = parseInt(result.get('left')) - parseInt(bomb.get('left'));
+            var tOffset = parseInt(result.get('top')) - parseInt(bomb.get('top'));
+            bomb.set({bar3_value: result.get('id'), bar2_value: (bomb.get('bar2_value') == '' || !isNum(bomb.get('bar2_value')) ? 10 : bomb.get('bar2_value')), bar3_max: lOffset + '|' + tOffset, showplayers_bar1: false, showplayers_bar2: false, showplayers_bar3: false, playersedit_bar1: false, playersedit_bar2: false, playersedit_bar3: false, playersedit_name: false});
+            result.set({layer: 'walls', bar3_value: bomb.get('id'), showplayers_bar1: false, showplayers_bar2: false, showplayers_bar3: false, showplayers_name: false, playersedit_bar1: false, playersedit_bar2: false, playersedit_bar3: false, playersedit_name: false});
+            state['TimeBomb'].bombs.push(bomb.get('id'));
+
+            showDialog('Success', '"' + bomb.get('name') + '" was created successfully! When ready to trigger the countdown (add it to the Turn Tracker), use <span style=\'' + styles.code + '\'>!bomb add</span> with the bomb token selected.', msg.who);
+        } else {
+            var err = "Couldn't find a required piece:<ul>"
+                + (typeof bomb == 'undefined' ? '<li>A token named "Bomb".</li>' : '')
+                + (typeof result == 'undefined' ? '<li>A token named "Result".</li>' : '')
+                + '</ul>';
+            showDialog('Error', err, msg.who);
+        }
+    },
+
+    commandAdd = function (msg) {
+        var bomb = getObj(msg.selected[0]._type, msg.selected[0]._id);
+        var to = getTurnOrder();
+        if (bomb && _.find(state['TimeBomb'].bombs, function (x) { return x == bomb.get('id'); }) && !_.find(to, function (y) { return y.id == bomb.get('id'); })) {
+            to.push({
+                id: bomb.get('id'),
+                pr: bomb.get('bar2_value'),
+                formula: -1
+            });
+            setTurnOrder(to);
+        } else {
+            var err = (_.find(to, function (x) { return x.id == bomb.get('id'); })) ? '"' + ((bomb) ? bomb.get('name') : 'unknown') + '" is already in the Turn Tracker' : 'Your Bomb must be created before it can be added to the Turn Tracker.';
+            showDialog('Error', err, msg.who);
+        }
+    },
+
+    commandReset = function (msg) {
+        var parms = msg.content.split(/\s+/i);
+        var token = getObj('graphic', parms[2]), bomb, result;
+        if (token) {
+                bomb = token;
+                result = getObj('graphic', bomb.get('bar3_value'));
+        }
+
+        if (bomb && result) {
+            var lOffset = parseInt(result.get('left')) - parseInt(bomb.get('left'));
+            var tOffset = parseInt(result.get('top')) - parseInt(bomb.get('top'));
+            result.set({layer: 'walls'});
+            bomb.set({layer: 'objects', bar3_value: result.get('id'), bar3_max: lOffset + '|' + tOffset, showplayers_bar1: false});
+            state['TimeBomb'].bombs.push(bomb.get('id'));
+            showDialog('Bomb Reset', '"' + bomb.get('name') + '" has been successfully reset. When ready to trigger the countdown (add it to the Turn Tracker), use <span style=\'' + styles.code + '\'>!bomb add</span> with the bomb token selected.', msg.who);
+            var bomb = getObj('graphic', result.get('bar3_value'));
+        } else {
+            var err = "Couldn't find a required piece:<ul>"
+                + (typeof bomb == 'undefined' ? '<li>A token named "Bomb".</li>' : '')
+                + (typeof result == 'undefined' ? '<li>A token named "Result".</li>' : '')
+                + '</ul>';
+            showDialog('Error', err, msg.who);
+        }
+    },
+
+    commandHideResult = function (msg) {
+        var parms = msg.content.split(/\s+/i);
+        var result = getObj('graphic', parms[2]);
+        if (result) {
+            result.set({layer: 'walls'});
+        }
+    },
+
+    commandDestroy = function (msg) {
+        if (_.size(msg.selected) == 0) {
+            showDialog('Error', 'You must select a valid Bomb "Result" token.', msg.who);
+            return;
+        }
+        var token = getObj(msg.selected[0]._type, msg.selected[0]._id);
+        if (token && (token.get('bar1_value') == 'Bomb' || token.get('bar1_value') == 'Result')) {
+            var bomb, result;
+            if (token.get('bar1_value') == 'Bomb') {
+                bomb = token;
+                result = getObj('graphic', bomb.get('bar3_value'));
+            } else {
+                result = token;
+                bomb = getObj('graphic', result.get('bar3_value'));
+            }
+            result.set({layer: 'objects', bar3_value: '', bar3_max: ''});
+            bomb.set({layer: 'objects', bar3_value: '', bar3_max: ''});
+            state['TimeBomb'].bombs = _.filter(state['TimeBomb'].bombs, function (x) { return x !== bomb.get('id') });
+            showDialog('Bomb Destroyed', '"' + bomb.get('name') + '" has been successfully destroyed.', msg.who);
+        } else {
+            showDialog('Error', 'The selected token was not a Bomb token.', msg.who);
+        }
+    },
+
+    getTurnOrder = function () {
+        return (Campaign().get('turnorder') === '') ? [] : Array.from(JSON.parse(Campaign().get('turnorder')));
+    },
+
+    setTurnOrder = function (to) {
+        // Make sure Turn Tracker is open first
+        if (Campaign().get('initiativepage') == false)
+        Campaign().set({initiativepage: Campaign().get('playerpageid')});
+        Campaign().set('turnorder', JSON.stringify(to));
+    },
+
+    getCurrentTurn = function () {
+        return _.first(getTurnOrder());
+    },
+
+    processGMNotes = function (notes) {
+        var retval, text = unescape(notes).trim();
+        if (text.search('{template:') != -1) {
+            text = removeFormatting(text);
+            text = text.replace('&amp;{template', '&{template');
+        }
+        return text;
+    },
+
+    removeFormatting = function (html) {
+        html = html.replace(/<p[^>]*>/gi, '<p>').replace(/\n(<p>)?/gi, '</p><p>').replace(/<br>/gi, '</p><p>').replace(/<\/?(span|div|pre|img|code|a|b|i|h1|h2|h3|h4|h5|hr)[^>]*>/gi, '');
+        if (html != '' && /<p>.*?<\/p>/g.test(html)) {
+            html = html.match(/<p>.*?<\/p>/g).map( l => l.replace(/^<p>(.*?)<\/p>$/,'$1'));
+            html = html.join(/\n/);
+        }
+        return html;
+    },
+
+    rollDice = function (exp) {
+        exp = exp.split(/\D/gi);
+        var roll, num = (exp[0]) ? parseInt(exp[0]) : 1,
+        die = (exp[1]) ? parseInt(exp[1]) : 6,
+        plus = (exp[2]) ? parseInt(exp[2]) : 0;
+        roll = (num == 1) ? randomInteger(die) : randomInteger(die * num - (num - 1)) + (num - 1);
+        return roll + plus;
+    },
+
+    isNum = function (txt) {
+        var nr = /^\d+$/;
+        return nr.test(txt);
+    },
+
+    commandHelp = function (msg) {
+        var message = '<span style=\'' + styles.code + '\'>!bomb help</span><br>Sends this dialog to the chat window.<br><br>';
+        message += 'More information here...';
+        showDialog('TimeBomb Help', message, msg.who);
+    },
+
+    showDialog = function (title, content, whisperTo = '') {
+        var gm = /\(GM\)/i;
+        title = (title == '') ? '' : '<div style=\'' + styles.title + '\'>' + title + '</div>';
+        var body = '<div style=\'' + styles.box + '\'>' + title + '<div>' + content + '</div></div>';
+        if (whisperTo.length > 0) {
+            whisperTo = '/w ' + (gm.test(whisperTo) ? 'GM' : '"' + whisperTo + '"') + ' ';
+            sendChat('TimeBomb', whisperTo + body, null, {noarchive:true});
+        } else  {
+            sendChat('TimeBomb', body);
+        }
+    },
+
+    checkTimer = function () {
+        var curr = getCurrentTurn();
+        if (curr && curr.id !== 'undefined' && _.find(state['TimeBomb'].bombs, function (x) { return x == curr.id; }) && curr.pr == 0) {
+            var bomb = getObj('graphic', curr.id);
+            if (bomb) {
+                var result = getObj('graphic', bomb.get('bar3_value')),
+                    offsets = bomb.get('bar3_max').split('|');
+                var left = parseInt(bomb.get('left')) + parseInt(offsets[0]),
+                    top = parseInt(bomb.get('top')) + parseInt(offsets[1]);
+
+                result.set({left: left, top: top});
+                _.delay(function () {
+                    var effect = processGMNotes(bomb.get('gmnotes'));
+                    if (effect !== '') showDialog('', effect);
+
+                    bomb.set({layer: 'walls'});
+                    if (result.get('bar1_max') == 'false' || result.get('bar1_max') == 'tokens' || result.get('bar1_max') == 'objects') {
+                        result.set({layer: 'objects'});
+                        toFront(result);
+                    } else {
+                        result.set({layer: 'map'});
+                    }
+
+                    var nto = _.filter(getTurnOrder(), function (obj) { return obj.id !== bomb.get('id'); });
+                    setTurnOrder(nto);
+
+                    state['TimeBomb'].bombs = _.filter(state['TimeBomb'].bombs, function (x) { return x !== bomb.get('id') });
+
+                }, 350);
+
+                _.delay(function () {
+                    showDialog('', '"' + bomb.get('name') + '" has been detonated.<div style=\'' + styles.buttonWrapper + '\'><a style=\'' + styles.button + '\' href="!bomb hide ' + result.get('id') + '" title="Hide Detonation Token">Hide</a> <a style=\'' + styles.button + '\' href="!bomb reset ' + bomb.get('id') + '" title="Reset Bomb">Reset</a></div>', 'GM');
+                }, 500);
+
+            } else showDialog('', 'Bomb not found!', 'GM');
+        }
+    },
+
+    //---- PUBLIC FUNCTIONS ----//
+
+    registerEventHandlers = function () {
+		on('chat:message', handleInput);
+        on("change:campaign:turnorder", checkTimer);
+	};
+
+    return {
+		checkInstall: checkInstall,
+		registerEventHandlers: registerEventHandlers
+	};
+}());
+
+on("ready", function () {
+    TimeBomb.checkInstall();
+    TimeBomb.registerEventHandlers();
+});
